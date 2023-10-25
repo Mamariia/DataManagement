55ac9493d856b59c2f78e9f73556a0238f3c8085
Mamariia <17mashadudina@gmail.com>
2023-10-25 21:14:39 +0300

    .

---
+++ DataManagement2/Controllers/HumanController.cs
@@ -0,0 +1,62 @@
+﻿using DataManagement1;
+using Microsoft.AspNetCore.Mvc;
+
+[Route("api/[controller]")]
+[ApiController]
+public class MyEntityController : ControllerBase
+{
+    private readonly IRepository<Person> _repository;
+
+    public MyEntityController(IRepository<Person> repository)
+    {
+        _repository = repository;
+    }
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        var persons = _repository.GetAll();
+        return Ok(persons);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetById(int id)
+    {
+        var entity = _repository.GetById(id);
+
+        if (entity == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(entity);
+    }
+
+    [HttpPost]
+    public IActionResult AddEntity([FromBody] Person person)
+    {
+        if (person == null)
+        {
+            return BadRequest("Entity cannot be null");
+        }
+
+        _repository.Add(person);
+
+        return CreatedAtAction(nameof(GetById), new { id = person.Id }, person);
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult DeleteEntity(int id)
+    {
+        var person = _repository.GetById(id);
+
+        if (person == null)
+        {
+            return NotFound();
+        }
+
+        _repository.Delete(id);
+
+        return NoContent();
+    }
+}
